15a5c3f7aa0680f720e369f4ef0b332da9839b3a
RAbeyrathna <rahal@res-network.com>
2024-04-15 20:12:39 +1000

    docs: Add documentation to dice roll functions

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,12 +1,23 @@
 
 
+/**
+ * Generate a random between 1 and a given size.
+ *
+ * @param {number} [diceSize=6] - Number that represents the number of sides on the dice
+ * @returns {*} - A random number between 1 and the given number of sides
+ */
 function rollDice(diceSize = 6) {
     let rollResult = Math.floor(Math.random() * diceSize) + 1;
     
     return rollResult;
 }
 
-
+/**
+ * Generate a random between 1 and a given size twice, identifies the highest number of the two.
+ *
+ * @param {number} [diceSize=6] - Number that represents the number of sides on the dice
+ * @returns {*} - A random number between 1 and the given number of sides twice, identifying the highest number of the two.
+ */
 function rollDiceWithAdvantage(diceSize = 6){
     let result = {
         finalResult: null,
@@ -24,7 +35,12 @@ function rollDiceWithAdvantage(diceSize = 6){
 
 }
 
-
+/**
+ * Generate a random between 1 and a given size twice, identifies the smallest number of the two.
+ *
+ * @param {number} [diceSize=6] - Number that represents the number of sides on the dice
+ * @returns {*} - A random number between 1 and the given number of sides twice, identifying the smallest number of the two.
+ */
 function rollDiceWithDisadvantage(diceSize = 6){
     let result = {
         finalResult: null,
