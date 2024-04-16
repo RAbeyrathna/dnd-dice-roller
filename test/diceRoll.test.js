7e5ceff686dbdfdf5e3cc2af8c39bcafdfdd6f69
RAbeyrathna <rahal@res-network.com>
2024-04-16 15:46:45 +1000

    test: Add coverage test command and test custom sides on rollDice

--- test/diceRoll.test.js
+++ test/diceRoll.test.js
@@ -1,12 +1,12 @@
-const { rollDice } = require("../src/diceRolling")
+const { rollDice } = require("../src/diceRolling");
 
-beforeEach(() => { 
-    console.log("A test has been started!")
-});
+// beforeEach(() => { 
+//     console.log("A test has been started!")
+// });
 
-afterAll(() => {
-    console.log("All tests have been completed!")
-});
+// afterAll(() => {
+//     console.log("All tests have been completed!")
+// });
 
 describe('As a casual gamer...', () => { 
     
@@ -18,6 +18,52 @@ describe('As a casual gamer...', () => {
         expect(result).toBeLessThanOrEqual(6);
 
 
-     })
+     });
 
- })
+ });
+
+ describe('As a dungeon master...', () => { 
+    describe('...I want to roll a vareity of dice sizes, such as...', () => { 
+        test('D6', () => { 
+            let result = rollDice();
+
+            expect(result).toBeGreaterThan(0);
+            expect(result).toBeLessThanOrEqual(6);
+
+         });
+
+         test('D8', () => { 
+            let result = rollDice(8);
+
+            expect(result).toBeGreaterThan(0);
+            expect(result).toBeLessThanOrEqual(8);
+
+         });
+
+         test('D10', () => { 
+            let result = rollDice(10);
+
+            expect(result).toBeGreaterThan(0);
+            expect(result).toBeLessThanOrEqual(10);
+
+         });
+
+         test('D12', () => { 
+            let result = rollDice(12);
+
+            expect(result).toBeGreaterThan(0);
+            expect(result).toBeLessThanOrEqual(12);
+
+         });
+
+         test('D20', () => { 
+            let result = rollDice(20);
+
+            expect(result).toBeGreaterThan(0);
+            expect(result).toBeLessThanOrEqual(20);
+
+         });
+     });
+    
+
+  });
